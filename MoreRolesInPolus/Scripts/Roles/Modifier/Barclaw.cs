3e6e2be82f7dcf24bba9bd7123d78df0d91fd864
junjun1z <admin@mybest-seat.com>
2026-01-07 23:08:45 +0900

    Barclawのファイルと基本的なコードを作成

---
+++ MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreRolesInPolus.Roles.Modifier;
+
+public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableModifier
+{
+
+
+    private Barclaw() : base("barclaw", "barclaw", new(180, 0, 0))
+    {
+
+    }
+
+    static public Barclaw MyRole = new Barclaw();
+    RuntimeModifier RuntimeAssignableGenerator<RuntimeModifier>.CreateInstance(GamePlayer player, int[] arguments) => new Instance(player);
+    public class Instance : RuntimeAssignableTemplate, RuntimeModifier
+    {
+        DefinedModifier RuntimeModifier.Modifier => MyRole;
+        public Instance(GamePlayer player) : base(player)
+        {
+        }
+
+        void RuntimeAssignable.OnActivated() { }
+
+        Dictionary<Player, (DefinedRole role, int roleCount)> roleMap = [];
+
+
+        int numOfDeadBody = 0;
+
+        [Local]
+        void OnDeadBodyGenerated(DeadBodyInstantiateEvent evt)
+        {
+            if (AmOwner)
+            {
+                numOfDeadBody++;
+
+                NebulaAPI.CurrentGame?.GetModule<TitleShower>()?.SetText(
+                    numOfDeadBody.ToString(), // int を string に変換
+                    new(100, 100, 100),
+                    5.5f,
+                    true
+                );
+            }
+        }
+
+
+        void OnPlayerRevived(PlayerReviveEvent ev)
+        {
+
+        }
+
+    }
+}
