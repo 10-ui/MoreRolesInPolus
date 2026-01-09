297d979fa56ccb51521aa9d5b076e53915c99f1a
junjun1z <admin@mybest-seat.com>
2026-01-09 23:15:23 +0900

    Meetingに入るようにした

--- MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
+++ MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
@@ -17,7 +17,10 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
     static public Barclaw MyRole = new Barclaw();
     RuntimeModifier RuntimeAssignableGenerator<RuntimeModifier>.CreateInstance(GamePlayer player, int[] arguments) => new Instance(player);
 
-    static private readonly IntegerConfiguration numOfDeadRequired = NebulaAPI.Configurations.Configuration("options.role.barclaw.numOfDeadRequired", (1, 5), 2);
+    static private readonly IntegerConfiguration NumOfDeadRequired = NebulaAPI.Configurations.Configuration("options.role.barclaw.numOfDeadRequired", (1, 5), 2);
+    static private readonly BoolConfiguration ShowMeetingInFlash = NebulaAPI.Configurations.Configuration("options.role.bait.showKillFlash", false);
+    static private readonly FloatConfiguration MeetingDelayOption = NebulaAPI.Configurations.Configuration("options.role.bait.reportDelay", (0f, 5f, 0.5f), 0f, FloatConfigurationDecorator.Second);
+    static private readonly FloatConfiguration MeetingDelayDispersionOption = NebulaAPI.Configurations.Configuration("options.role.bait.reportDelayDispersion", (0f, 10f, 0.25f), 0.5f, FloatConfigurationDecorator.Second);
 
 
     public class Instance : RuntimeAssignableTemplate, RuntimeModifier
@@ -56,13 +59,14 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
                     true
                 );
 
-                if (numOfKillCount >= numOfDeadRequired && !myDead)
+                if (numOfKillCount >= NumOfDeadRequired && !myDead)
                 {
                     AmongUsUtil.PlayQuickFlash(MyRole.UnityColor);
-                    //緊急会議が始まる予定
+                    CallMeeting(MyPlayer);
                 }
             }
         }
+
 
         [Local]
         void OnPlayerRevived(PlayerReviveEvent ev)
@@ -87,15 +91,7 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
             }
         }
 
-        void OnMeetingStart(MeetingStartEvent ev)
-        {
-            if (AmOwner)
-            {
-                nowMeeting = true;
-                numOfKillCount = 0;
-                deadPlayer.Clear();
-            }
-        }
+
         void OnMeetingEnd(MeetingEndEvent ev)
         {
             if (AmOwner)
@@ -106,5 +102,34 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
             }
 
         }
+
+
+        void OnMeetingPreStart(MeetingPreStartEvent ev)
+        {
+            if (AmOwner)
+            {
+                nowMeeting = true;
+                numOfKillCount = 0;
+                deadPlayer.Clear();
+            }
+
+        }
+
+        [NebulaRPC]
+        public static void CallMeeting(GamePlayer p)
+        {
+            if (AmongUsClient.Instance.AmHost)
+            {
+                var player = PlayerControl.AllPlayerControls.GetFastEnumerator().FirstOrDefault(c => c.PlayerId == p.PlayerId);
+                MeetingRoomManager.Instance.AssignSelf(player, null);
+                if (GameManager.Instance.CheckTaskCompletion())
+                {
+                    return;
+                }
+                DestroyableSingleton<HudManager>.Instance.OpenMeetingRoom(player);
+                player.RpcStartMeeting(null);
+            }
+        }
+
     }
 }
