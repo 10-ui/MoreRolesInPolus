0a837221a750f11c5425c318484d5847f6273a61
junjun1z <admin@mybest-seat.com>
2026-01-11 03:31:05 +0900

    ごめんなさい

--- MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
+++ MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
@@ -65,7 +65,7 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
 
                 if (numOfKillCount >= numOfDeadRequired && !myDead && numOfMaxButton >= numOfButtonCount)
                 {
-                    if(enableAfterSecondTurn && 2 <= nowTurn) return;
+                    if(enableAfterSecondTurn && nowTurn <= 2) return;
                     if(showMeetingInFlash) AmongUsUtil.PlayQuickFlash(MyRole.UnityColor);
 
                     float t = Mathn.Max(0.1f, meetingDelayOption) + meetingDelayDispersionOption * (float)System.Random.Shared.NextDouble();
