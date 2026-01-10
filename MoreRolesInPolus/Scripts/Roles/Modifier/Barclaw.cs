918638693238115a1fe4f7c6411073afbf3ecab5
junjun1z <admin@mybest-seat.com>
2026-01-11 03:22:16 +0900

    ばーくろー発動しないよ～

--- MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
+++ MoreRolesInPolus/Scripts/Roles/Modifier/Barclaw.cs
@@ -61,9 +61,9 @@ public class Barclaw : DefinedAllocatableModifierTemplate, DefinedAllocatableMod
                 }
 
                 numOfKillCount++;
-                deadPlayer.Add(ev.Dead);
+                deadPlayer.Add(ev.Dead); numOfMaxButton
 
-                if (numOfKillCount >= numOfDeadRequired && !myDead && numOfButtonCount >= numOfMaxButton)
+                if (numOfKillCount >= numOfDeadRequired && !myDead && numOfMaxButton >= numOfButtonCount)
                 {
                     if(enableAfterSecondTurn && 2 <= nowTurn) return;
                     if(showMeetingInFlash) AmongUsUtil.PlayQuickFlash(MyRole.UnityColor);
